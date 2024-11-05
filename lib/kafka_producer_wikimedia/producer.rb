2eaefa19251dbf6b169442771350e7c7c299ebcd
Luke McFarlane <luke.mcfarlane@fluxfederation.com>
2024-11-06 09:51:47 +1300

    Produce test kafka messages

--- lib/kafka_producer_wikimedia/producer.rb
+++ lib/kafka_producer_wikimedia/producer.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
+require 'rdkafka'
+
 module KafkaProducerWikimedia
   class Producer
+
+    TOPIC = 'wikimedia.recentchange'
+
     def initialize(config)
       @config = config
     end
 
     def produce_from_stream
-      puts config.bootstrap_server
+      kafka_config = {
+        'bootstrap.servers' => config.bootstrap_server,
+      }
+      producer = Rdkafka::Config.new(kafka_config).producer
+      delivery_handles = []
+      i = 0
+
+      loop do
+        puts "producing message #{i}"
+        delivery_handles << producer.produce(
+          topic: TOPIC,
+          payload: "test #{i}",
+          key: "message_#{i}",
+        )
+
+        i += 1
+        sleep 1
+      end
     end
 
     def self.produce_from_stream
